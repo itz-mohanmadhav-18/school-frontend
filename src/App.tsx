2c3a346f3ef76b5a3a6daa2e4263b0d165271675
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-07-23 18:35:53 +0530

    added support for dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import { apolloClient } from '@/lib/apollo-client';
-import { AuthProvider } from '@/context/AuthProvider'; // Updated import
+import { AuthProvider } from '@/context/AuthProvider';
 import { AppRoutes } from '@/routes';
+import { ThemeProvider } from '@/components/theme-provider';
 
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
       <AuthProvider>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </ThemeProvider>
       </AuthProvider>
     </ApolloProvider>
   );
